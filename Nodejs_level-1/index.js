614ca940a7cf774b6dccdafc0e6d6f9bb2e88b47
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2024-07-06 18:57:31 +0530

    basic nodejs

--- Nodejs_level-1/index.js
+++ Nodejs_level-1/index.js
@@ -1,64 +1,99 @@
-const fs = require("fs");
+const fs=require("fs")
+function readfile(file){
+  // readfile
+      fs.readFile(file, { encoding: 'utf-8' }, (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(data);
+        }
+      })
+  }
 
-// readfile
-    fs.readFile("./test.txt", { encoding: 'utf-8' }, (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(data);
+
+  // append text
+  function appendfile(file){
+
+    fs.appendFile(file,'\nthis is another msg which i appending there\n',(err)=>{
+      if(err){
+        console.log(err)
+      }else{
+        console.log("Content appended to the file 'test.txt'")
       }
     })
-    
-// append text
-   fs.appendFile("./test.txt",'\nthis is another msg which i appending there\n',(err)=>{
-    if(err){
-      console.log(err)
-    }else{
-      console.log("Content appended to the file 'test.txt'")
-    }
-  })
-  
+  }
+
   // deleted file
-  fs.unlink('./test2.txt',(err)=>{
+  function deletefile(file){
+    fs.unlink(file,(err)=>{
+      if(err){
+        console.log(err)
+      }else{
+        console.log(" file 'test.txt' deleted")
+      }
+      
+    })
+  }
+
+
+  // createing new file
+function createfile(file){
+
+  fs.writeFile(file,'hello newfile',(err)=>{
     if(err){
       console.log(err)
     }else{
-      console.log(" file 'test2.txt' deleted")
+      console.log("File 'newfile' created")
     }
     
   })
+}
+   
+   // reanaming file name
+   function renamefile(file){
+
+     fs.rename(file,'./new.txt',(err)=>{
+       
+       if(err){
+         console.log(err)
+        }else{
+          console.log("File 'test.txt' renamed to 'new.txt'")
+        }
+      })
+    }
+
+   // list directory
+   function listdirec(file){
+     fs.readdir(file,(err)=>{
+       
+       if(err){
+         console.log(err)
+        }else{
+          console.log("A list of all files and directories in the current directory")
+        }
+        
+      })
+    }
+      
+  const operation = process.argv[2];
+  const file = process.argv[3];
+  if(operation=="read"){
+      readfile(file)
+  }
+  else if(operation=="append"){
+      appendfile(file)
+  }
+  else if(operation=="delete"){
+      deletefile(file)
+  }
+  else if(operation=="create"){
+      createfile(file)
+  }
+  else if(operation=="rename"){
+      renamefile(file)
+  }
+  else if(operation=="list"){
+      listdirec(file)
+  }
+
   
-  // createing new file
-
-  fs.writeFile('newfile','hello newfile',(err)=>{
-     if(err){
-       console.log(err)
-     }else{
-       console.log("File 'newfile' created")
-     }
-     
-    })
-    
-    // reanaming file name
-    fs.rename('./test1.txt','./new.txt',(err)=>{
-      
-      if(err){
-        console.log(err)
-      }else{
-        console.log("File 'test.txt' renamed to 'new.txt'")
-      }
-    })
-
-    // list directory
-    fs.readdir('./index.js',(err)=>{
-      if(err){
-        console.log(err)
-      }else{
-        console.log("A list of all files and directories in the current directory")
-      }
-      
-   })
-
-    
-
-
