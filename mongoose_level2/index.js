fa14ec75bfa0104bea655779f0f506ebb2a5916c
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2024-08-01 10:34:56 +0530

    ...

--- mongoose_level2/index.js
+++ mongoose_level2/index.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const connection=require("./config/db")
-const GlassModel=require("./models/glass.model")
+const GlassModel=require("./models/glassmodel")
 
 const server=express();
 const PORT=3080;
